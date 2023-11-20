31c010c883f7525042abc78c1fa5003d6aa618af
Sergio Mendes <sergiomendes@MacBook-Air-de-Sergio.local>
2023-11-20 12:08:08 -0300

    Concat name

--- desafio-xCode.swiftpm/ContentView.swift
+++ desafio-xCode.swiftpm/ContentView.swift
@@ -3,23 +3,14 @@ import SwiftUI
 
 
 struct ContentView: View {
-    
-    func concatName() {
-        let name = "Steve"
-        var Jobs: String?
-        
-        if let subName = Jobs {
-            print(name + subName)
-        }else{
-            print(name + "Wozniak")
-        }
-    }
+    let name = "Steve"
+    var subName = "Jobs"
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text(concatName())
+            Text(name + " " + subName)
         }
     }
 }
